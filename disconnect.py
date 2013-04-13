55e2802906556f32c6b3ceec5514293ede78ebdc
aviot <aviotster@gmail.com>
2013-04-13 12:33:03 +0800

    Update disconnect.py

--- disconnect.py
+++ disconnect.py
@@ -3,10 +3,10 @@ import os,time
 # call rasdial disconnect connect.
 os.system('rasdial /Disconnect')
 # call rasdial connect entyname account passport.
-result = os.system('rasdial adsl aviot 32113211')
+result = os.system('rasdial entyname account passport')
 # if connect failed ,wait 10 second and retry 10 times.
 count = 10
 while result == 0 and count >0:
 	time.sleep(10)
-	result=os.system('rasdial adsl aviot 32113211')
+	result=os.system('rasdial enty account passport')
 	count -= 1
